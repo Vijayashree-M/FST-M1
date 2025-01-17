4ea439ea5d724ef3458fb7efb3f962acfdfb22c2
Vijayashree-M <vijayasm@in.ibm.com>
2025-01-18 02:52:40 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunner1.java
@@ -0,0 +1,16 @@
+package runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "features",
+    glue = {"stepdefinitions"},
+    tags = "@activity1"
+)
+
+public class ActivitiesRunner1 {
+    //empty
+}
